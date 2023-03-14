46f5612b8e7bd7b6826bff99eacbb7b1a4b4d292
mangtri1 <127836696+mangtri1@users.noreply.github.com>
2023-03-14 17:04:17 +0800

    sesi 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan HTML sesi 1</title>
+</head>
+<body>
+    
+    <h1 style="text-align: center;"> belajar web programing<?="<sup>80</sup>"?></h1>
+
+    <script>
+        document.write("<h3>menggunakan script javascript<h3>");
+    </script>
+
+    <?php
+        echo "<h3>menggunakan script PHP</h3>";
+    ?>
+
+</body>
+</html>
